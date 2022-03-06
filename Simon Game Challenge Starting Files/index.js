b11c3fff921d97936f7c00400f0adb3837115918
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-06 14:05:51 +0530

    button sequence

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,19 +1,72 @@
 //alert("Click a key to start the a game");
 
-var buttonColors = ['green','red','yellow','blue']
+var buttonColors = ['green','red','yellow','blue'];
+var buttonSequence = [];
+var userButtonSequence = [];
+var level = 0;
 
 $("h1").click(function () { 
     console.log("hey");
 });
 
 function nextSequence(){
+    $("h1").text("Level "+level);
+    level++;
     var randomNumber = Math.round((Math.random())*3);
     var randomColor = buttonColors[randomNumber]
-    $("."+randomColor).addClass("pressed");
-    setTimeout(function(){
-        $("."+randomColor).removeClass("pressed");
-    },100)
-    return randomColor;
+    flashButton("."+randomColor);
+    playSound(randomColor);
+    var chosenColor = $("."+randomColor).attr('id');
+    buttonSequence.push(chosenColor);
+    return buttonSequence;
 }
 
-var audio = new Audio("../Simon Game Challenge Starting Files/sounds/blue.mp3")
+function flashButton(button){
+    $(button).addClass("pressed");
+    setTimeout(function(){
+        $(button).removeClass("pressed");
+    },100);
+}
+
+
+function playSound(color){
+
+    switch(color){
+        case "red": 
+            var audio = new Audio("../Simon Game Challenge Starting Files/sounds/red.mp3");
+            audio.play();
+        break;
+        case "yellow": 
+            var audio = new Audio("../Simon Game Challenge Starting Files/sounds/yellow.mp3");
+            audio.play();
+        break;
+        case "green": 
+            var audio = new Audio("../Simon Game Challenge Starting Files/sounds/green.mp3");
+            audio.play();
+        break;
+        case "blue": 
+            var audio = new Audio("../Simon Game Challenge Starting Files/sounds/blue.mp3");
+            audio.play();
+        break;
+    }
+
+}
+
+
+function handleClick(currentButton) {
+    
+ }
+
+$(".btn").click(function () {
+    var clickedButton = this.id;
+    flashButton("."+clickedButton);
+    playSound(clickedButton);
+});
+
+
+
+$(document).one('keydown',function (e) { 
+    nextSequence();
+
+});
+
